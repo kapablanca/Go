3d2b4b6bd650da12e435d2dc2682086ce145340c
Stathis Kazanakis <skazanak@noreply.platform.zone01.gr>
2024-04-02 11:44:53 +0000

    Update 'myfamily.sh'

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-curl https://platform.zone01.gr/assets/superhero/all.json | jq -r -a '.[] | select (.id=='$HERO_ID') | .connections.relatives'
+curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r -a '.[] | select (.id=='$HERO_ID') | .connections.relatives'
