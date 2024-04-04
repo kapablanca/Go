5b8c0e005087c1d6d7631d54898badc540d72ee8
Stathis Kazanakis <skazanak@noreply.platform.zone01.gr>
2024-04-04 20:24:24 +0000

    Update 'printcombn.go'

--- printcombn.go
+++ printcombn.go
@@ -1,36 +1,36 @@
 package piscine
 
 import (
-    "github.com/01-edu/z01"
+	"github.com/01-edu/z01"
 )
 
 func PrintCombN(n int) {
-    generateCombinations("", 0, n)
+	generateCombinations("", 0, n)
 }
 
 func generateCombinations(prefix string, start, n int) {
-    if len(prefix) == n {
-        for _, r := range prefix {
-            z01.PrintRune(rune(r))
-        }
-        // Determine if this is the last combination to decide whether to print a comma and space
-        if prefix < "123456789"[9-n:] {
-            z01.PrintRune(',')
-            z01.PrintRune(' ')
-        } else {
-            z01.PrintRune('\n') // Print a newline at the end
-        }
-        return
-    }
+	if len(prefix) == n {
+		for _, r := range prefix {
+			z01.PrintRune(rune(r))
+		}
+		// Determine if this is the last combination to decide whether to print a comma and space
+		if prefix < "123456789"[9-n:] {
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		} else {
+			z01.PrintRune('\n') // Print a newline at the end
+		}
+		return
+	}
 
-    for i := start; i <= 9; i++ {
-        newPrefix := prefix + intToString(i) // Use the custom int to string conversion
-        generateCombinations(newPrefix, i+1, n)
-    }
+	for i := start; i <= 9; i++ {
+		newPrefix := prefix + intToString(i) // Use the custom int to string conversion
+		generateCombinations(newPrefix, i+1, n)
+	}
 }
 
 // intToString converts an integer to its string representation.
 // This version directly constructs the string expected for single digits, leveraging rune conversion.
 func intToString(num int) string {
-    return string('0' + rune(num))
+	return string('0' + rune(num))
 }
