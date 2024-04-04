309aa54228487f2026101c3619b21cfdb5459667
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-04 10:14:22 +0300

    Changing the folder to mystery.

--- teacher.sh
+++ teacher.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-INTERVIEW=$(grep "SEE INTERVIEW"  the-final-cl-test/mystery/streets/Buckingham_Place | grep -oP '\d+')
+INTERVIEW=$(grep "SEE INTERVIEW" mystery/streets/Buckingham_Place | grep -oP '\d+')
 
 echo "$INTERVIEW"
 cat the-final-cl-test/mystery/interviews/interview-$INTERVIEW
