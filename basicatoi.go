aa7ec2ebe6b979702a4737929739c928556790ec
Stathis Kazanakis <skazanak@noreply.platform.zone01.gr>
2024-04-05 12:10:52 +0000

    Add 'basicatoi.go'

---
+++ basicatoi.go
@@ -0,0 +1,15 @@
+package piscine
+
+func BasicAtoi(s string) int {
+
+	var number int = 0
+
+	for _, num := range s {
+
+		var digit rune =  num - 48
+		number = number*10 + int(digit)
+	}
+
+	return number
+
+}
