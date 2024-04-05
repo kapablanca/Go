972af2b28c241eae843477fc1ccf7706207b9985
Stathis Kazanakis <skazanak@noreply.platform.zone01.gr>
2024-04-05 19:32:27 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -1,26 +1,29 @@
 package piscine
 
 func Atoi(s string) int {
-	var number int = 0
-	sign := s[0]
-	if (sign == '-' || sign == '+') && s[1] != '+' || s[1] != '-' {
-		s = s[1:]
-	} else {
+	if s == "" {
 		return 0
 	}
 
-	for _, num := range s {
+	number := 0
+	sign := 1
+	start := 0
 
-		var digit rune = num - 48
-		if int(digit) < 0 || int(digit) > 9 {
-			return 0
+	// Check if the string starts with a sign.
+	if s[0] == '-' || s[0] == '+' {
+		if s[0] == '-' {
+			sign = -1
+		}
+		start = 1
+	}
+
+	for i := start; i < len(s); i++ {
+		digit := s[i] - '0'
+		if digit < 0 || digit > 9 {
+			return 0 // Contains a non-digit character.
 		}
 		number = number*10 + int(digit)
 	}
 
-	if sign == '-' {
-		return number * (-1)
-	} else {
-		return number
-	}
+	return number * sign
 }
