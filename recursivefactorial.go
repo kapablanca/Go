336763f68c9ec36ce226b31839d8e1e5250d215b
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-08 14:57:31 +0300

    Trying to fix signal killed problem.

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,14 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 || nb == 21 {
+	if nb == 21 {
 		return 0
 	}
 	if nb == 0 {
 		return 1
 	}
-	return nb * RecursiveFactorial(nb-1)
+	if nb > 0 {
+		return nb * RecursiveFactorial(nb-1)
+	}
+	return 0
 }
