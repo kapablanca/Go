bc6422c702ea2097657ea2cab33b69e1b51eb74d
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-08 18:49:24 +0300

    Fixing result cases throughout the code.

--- isprime.go
+++ isprime.go
@@ -3,11 +3,13 @@ package piscine
 func IsPrime(nb int) bool {
 	result := true
 	if nb <= 1 {
+		result = false
 		return result
 	}
 	for i := 2; i < nb; i++ {
 		if nb%i == 0 {
 			result = false
+			return result
 		}
 	}
 	return result
