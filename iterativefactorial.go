04108276cb96cf0fbc7cae645e1bb0287de6ee5c
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-08 13:42:30 +0300

    Updating to handle maxvalue.

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,7 +5,7 @@ func IterativeFactorial(nb int) int {
 		return 0
 	}
 	var result int = 1
-	var maxInteger int = 9223372036854775807
+	var maxInteger int = 2147483647 * 2147483647
 
 	for i := 1; i <= nb; i++ {
 		if result > maxInteger {
