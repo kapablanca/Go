16eea0d3e247a85473732773320bb9449200a1b7
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-04-09 18:58:09 +0300

    Adding go-testing.

--- go-testing/test.go
+++ go-testing/test.go
@@ -3,8 +3,5 @@ package main
 import "piscine"
 
 func main() {
-	// Test PrintCombN function with different values of n
-	piscine.PrintCombN(1)
-	piscine.PrintCombN(3)
-	piscine.PrintCombN(9)
+	piscine.QuadA(5, 3)
 }
