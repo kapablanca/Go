a0e7284f7a1369c9f46948360a81a8dfd2296bbf
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-10 15:24:34 +0300

    First try.

---
+++ revparams/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args
+
+	if len(arguments) > 1 {
+		for i := len(arguments) - 1; i >= 0; i-- {
+
+			for _, letter := range arguments[i] {
+				z01.PrintRune(letter)
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
