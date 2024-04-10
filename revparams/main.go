83420318756213732716cafd68942d99117f1a9b
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-10 15:26:08 +0300

    Changing the index to not take 0 value.

--- revparams/main.go
+++ revparams/main.go
@@ -10,7 +10,7 @@ func main() {
 	arguments := os.Args
 
 	if len(arguments) > 1 {
-		for i := len(arguments) - 1; i >= 0; i-- {
+		for i := len(arguments) - 1; i > 0; i-- {
 
 			for _, letter := range arguments[i] {
 				z01.PrintRune(letter)
