84d3ab38802da9631e0e9afd2968fdb93b9cd467
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-04-11 11:44:08 +0300

    Finalized version of function.

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	arguments := os.Args[1:]
-	var letter rune
+
 	// checking first argument to implement capital letters if needeed
 	capital := false
 
@@ -21,14 +21,23 @@ func main() {
 	}
 	// checking each argument that is valid and printing
 	for _, nbr := range arguments {
-		if !piscine.IsNumeric(nbr) || nbr < "1" || nbr > "26" {
+		if !piscine.IsNumeric(nbr) {
 			z01.PrintRune(' ')
-		} else if !capital {
-			letter = int(nbr)
-			z01.PrintRune()
-		} else {
-			z01.PrintRune(rune)
-		}
-	}
 
+		} else {
+			digit := piscine.Atoi(nbr)
+
+			if digit < 1 || digit > 26 {
+				z01.PrintRune(' ')
+			} else {
+				if capital {
+					z01.PrintRune(rune(digit - 1 + 'A'))
+				} else {
+					z01.PrintRune(rune(digit - 1 + 'a'))
+				}
+			}
+
+		}
+
+	}
 }
