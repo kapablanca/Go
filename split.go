dce4d1ad134c79e6a01dceca8b66cb65fbaeb82e
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-04-14 09:16:10 +0300

    Solving some issues.

--- split.go
+++ split.go
@@ -3,55 +3,24 @@ package piscine
 func Split(s, sep string) []string {
 	var words []string
 
-	for i, word := range s {
+	for i := 0; i < len(s)-len(sep); {
+
+		j := len(sep)
+
+		if s[i:i+j] == sep {
+
+			if i != 0 {
+				words = append(words, s[:i])
+			}
+			s = s[i+j:]
+			i = 0
+		} else {
+			i++
+		}
+
 	}
+	if len(s) > 0 {
+		words = append(words, s)
+	}
+	return words
 }
-
-// var splitted []string
-// var word string
-
-// if len(sep) == 1 {
-// 	for i := 0; i < len(s); i++ {
-// 		if string(s[i]) == sep {
-// 			if i != 0 && string(s[i-1]) != sep {
-
-// 				splitted = append(splitted, word)
-// 				word = ""
-// 			} else {
-// 				word = ""
-// 			}
-// 		} else {
-
-// 			word += string(s[i])
-
-// 			if i == len(s)-1 {
-// 				splitted = append(splitted, word)
-// 				word = ""
-// 			}
-
-// 		}
-// 	}
-// } else {
-// 	for i := 0; i < len(s)-len(sep); i = i + len(sep) {
-// 		if string(s[i:i+len(s)]) == sep {
-// 			if i != 0 && string(s[i-len(sep):i]) != sep {
-
-// 				splitted = append(splitted, word)
-// 				word = ""
-// 			} else {
-// 				word = ""
-// 			}
-// 		} else {
-
-// 			word += string(s[i : i+len(s)])
-
-// 			if i == len(s)-len(sep)-1 {
-// 				splitted = append(splitted, word)
-// 				word = ""
-// 			}
-
-// 		}
-// 	}
-// }
-
-// return splitted
