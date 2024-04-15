9c88800c05ba7ed4489f8c6933012f5be36b33c3
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-15 14:06:41 +0300

    Printing with the right function.

--- point/main.go
+++ point/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 type point struct {
 	x int
@@ -12,10 +12,39 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func numberToString(nbr int) string {
+	var digits []rune
+	var digitsString string
+
+	mod := 1
+
+	for mod != 0 {
+		mod = nbr % 10
+		if mod == 0 {
+			continue
+		}
+		digits = append(digits, rune(mod)+48)
+		nbr /= 10
+	}
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		digitsString += string(digits[i])
+	}
+	return digitsString
+}
+
 func main() {
-	points := &point{}
+	points := point{}
+	points_point := &points
 
-	setPoint(points)
+	setPoint(points_point)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	x := numberToString(points.x)
+	y := numberToString(points.y)
+
+	sentence := "x = " + x + ", y = " + y + "\n"
+
+	for _, char := range sentence {
+		z01.PrintRune(char)
+	}
 }
