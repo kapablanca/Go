c329cf95b0d72117600099a7fff2518895bdd653
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-16 14:30:39 +0300

    Saving the sorting.

--- issorted.go
+++ issorted.go
@@ -11,12 +11,26 @@ func f(a, b int) int {
 }
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	var sorting_list []int
+	one := 0
+	negative_one := 0
+
 	for i := 0; i < len(a)-1; i++ {
 		for j := i + 1; j < len(a); j++ {
-			if f(a[i], a[j]) > 0 {
-				return false
-			}
+			sorting_list = append(sorting_list, f(a[i], a[j]))
 		}
+	}
+
+	for _, digit := range sorting_list {
+		if digit == 1 {
+			one = 1
+		}
+		if digit == -1 {
+			negative_one = 1
+		}
+	}
+	if one == 1 && negative_one == -1 {
+		return false
 	}
 	return true
 }
