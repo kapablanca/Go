397446b4c2ba0595d739bd3bb510419ad5c7cc47
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-18 18:04:08 +0300

    Changing the i to increment only when I find non zero value.

--- compact.go
+++ compact.go
@@ -3,9 +3,10 @@ package piscine
 func Compact(ptr *[]string) int {
 	count := 0
 
-	for i := 0; i < len(*ptr); i++ {
+	for i := 0; i < len(*ptr); {
 		if (*ptr)[i] != "" {
 			count++
+			i++
 		} else {
 			(*ptr) = append((*ptr)[:i], (*ptr)[i+1:]...)
 		}
