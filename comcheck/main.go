cae8856e4fab8a2b5a9cebdc41a405ba48dce8ed
stath <efstathiosgkazanakis@protonmail.com>
2024-04-19 01:34:21 +0300

    First try.

---
+++ comcheck/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	arguments := os.Args[1:]
+
+	for _, arg := range arguments {
+		if arg == "01" || arg == "galaxy" || arg == "galaxy 01" {
+			fmt.Println("Alert!!!")
+		}
+	}
+}
