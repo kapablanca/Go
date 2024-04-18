e03184d4b75e92449ec38bb8a10dd8055362d9aa
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-18 19:45:35 +0300

    Changing the limit of len string.

--- jumpover.go
+++ jumpover.go
@@ -6,7 +6,7 @@ func JumpOver(str string) string {
 		result = "\n"
 	} else {
 		for index, char := range str {
-			if (index+1)%3 == 0 && index+1 < len(str) {
+			if (index+1)%3 == 0 && index < len(str) {
 				result += string(char)
 			}
 		}
