03b345ee0609763d15985344e8c46e1fa1528299
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-18 17:00:27 +0300

    Changing function.

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,7 +1,26 @@
 package piscine
 
+func SplitOneSpace(str string) []string {
+	var words []string
+
+	for i := 0; i < len(str); {
+		if string(str[i]) == " " {
+
+			words = append(words, str[:i])
+			str = str[i+1:]
+			i = 0
+		} else {
+			i++
+		}
+	}
+	if len(str) > 0 {
+		words = append(words, str)
+	}
+	return words
+}
+
 func ShoppingSummaryCounter(str string) map[string]int {
-	items_slice := Split(str, " ")
+	items_slice := SplitOneSpace(str)
 
 	shopping_list := make(map[string]int)
 
