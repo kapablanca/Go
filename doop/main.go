ab37b31d320294fb303ad1698b6d89dd46eeb4de
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-04-20 01:10:38 +0300

    Trying to change the code but failed.

--- doop/main.go
+++ doop/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // Checking is string is numerical value
 func isNumber(s string) bool {
@@ -15,14 +18,16 @@ func isNumber(s string) bool {
 // Checking validity of operator
 func checkOperator(operator string) bool {
 
-	valid_operators := [...]string{"+", "-", "/", "*", "%"}
+	// valid_operators := [...]string{"+", "-", "/", "*", "%"}
+
+	valid_operators := [...]rune{'+', '-', '/', '*', '%'}
 
 	if len(operator) != 1 {
 		return false
 	}
 
 	for _, symbol := range valid_operators {
-		if operator == symbol {
+		if operator == string(symbol) {
 			return true
 		}
 	}
@@ -63,13 +68,18 @@ func multiply(n1, n2 int) int {
 
 func divide(n1, n2 int) int {
 	if n2 == 0 {
-		return 0
+		os.Stdout.WriteString("No division by 0\n")
+		os.Exit(0)
 	}
 	n := n1 / n2
 	return n
 }
 
 func modulo(n1, n2 int) int {
+	if n2 == 0 {
+		os.Stdout.WriteString("No modulo by 0\n")
+		os.Exit(0)
+	}
 	n := n1 % n2
 	return n
 }
@@ -93,8 +103,9 @@ func atoi(s string) int {
 	}
 	return number * sign
 }
+
 // Function to apply the selected operator
-func applyOperator(f func(int,int)int, n1, n2 int) int {
+func applyOperator(f func(int, int) int, n1, n2 int) int {
 
 	result := f(n1, n2)
 
@@ -102,14 +113,53 @@ func applyOperator(f func(int,int)int, n1, n2 int) int {
 
 }
 
+// Convert number to string
+func itoa(num int) string {
+	// Special case for zero
+	if num == 0 {
+		return "0"
+	}
+
+	// Check if the number is negative
+	isNegative := num < 0
+	if isNegative {
+		num = -num // Make the number positive for processing
+	}
+
+	// This will store the digits of the number.
+	digits := []byte{}
+
+	// Extract digits of the number
+	for num > 0 {
+		digit := num % 10
+		// Prepend digit to the slice (since we're reading the digits in reverse order)
+		digits = append([]byte{byte('0' + digit)}, digits...)
+		num /= 10
+	}
+
+	// If the number was negative, prepend a '-' sign
+	if isNegative {
+		digits = append([]byte{'-'}, digits...)
+	}
+
+	// Convert byte slice to string
+	return string(digits)
+}
+
 // Main Program
 func main() {
 
+	if len(os.Args) < 2 {
+		return
+	}
 	arguments := os.Args[1:]
+	fmt.Println("Processed Arguments:", arguments)
 
-	checkArgs(arguments)
+	if !checkArgs(arguments) {
+		return
+	}
 
-	valid_operators := [...]string{"+", "-", "/", "*", "%"}
+	valid_operators := [...]rune{'+', '-', '/', '*', '%'}
 
 	list_operations := []func(int, int) int{add, subtract, divide, multiply, modulo}
 
@@ -119,21 +169,21 @@ func main() {
 	operation_index := 0
 
 	for index, oper := range valid_operators {
-		if arguments[1] == oper {
+		if arguments[1] == string(oper) {
 			operation_index = index
 		}
 	}
 
 	if operation_index == 4 && n2 == 0 {
 		os.Stdout.WriteString("No modulo by 0")
-		return 
+		return
 	}
 
 	if operation_index == 2 && n2 == 0 {
-		os.Stdout.WriteString("No modulo by 0")
-		return 
+		os.Stdout.WriteString("No division by 0")
+		return
 	}
 
-	result := applyOperator((list_operations[operation_index], n1, n2))
-	os.Stdout.WriteString(result)
+	result := applyOperator(list_operations[operation_index], n1, n2)
+	os.Stdout.WriteString(itoa(result))
 }
