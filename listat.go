54fade837226a0f8b330ed49b28e0ba0c95f4115
stath <efstathiosgkazanakis@protonmail.com>
2024-04-23 01:04:54 +0300

    First try.

---
+++ listat.go
@@ -0,0 +1,30 @@
+package piscine
+
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
+
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
+
+func ListAt(l *NodeL, pos int) *NodeL {
+
+	if l == nil {
+		return nil
+	}
+
+	currentNode := l
+	index := 0
+
+	for currentNode != nil {
+		if pos == index {
+			return currentNode
+		}
+		currentNode = currentNode.Next
+		index++
+	}
+	return nil
+}
