df57b886ba2b7531299ecdf085972a5599bc0a51
stath <efstathiosgkazanakis@protonmail.com>
2024-04-24 00:37:15 +0300

    Updating the alphabet file.

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,7 +4,6 @@ import "github.com/01-edu/z01"
 
 // Function to print the Latin alphabet in lowercase letters
 func main() {
-
 	alphabetLowercase := "abcdefghijklmnopqrstuvwxyz"
 
 	for _, letter := range alphabetLowercase {
