e008cb72f1ee0a730c7559eaaef3543bfcd1f03d
stath <efstathiosgkazanakis@protonmail.com>
2024-04-24 23:28:47 +0300

    Update ultimatedivmod

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,8 +1,14 @@
 package piscine
 
+/* Function that divides the dereferenced value of a
+by the dereferenced value of b, stores the result of
+the division in the int which a points to and stores
+the remainder of the division in the int which b points to */
 func UltimateDivMod(a *int, b *int) {
-	q := *a / *b
-	r := *a % *b
-	*a = q
-	*b = r
+	if *b != 0 {
+		// Stores temporary the dereferenced value of a
+		temp := *a
+		*a = *a / *b
+		*b = temp % *b
+	}
 }
