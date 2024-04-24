07d35182be28c0739952bafcf3103d99874fb5b8
stath <efstathiosgkazanakis@protonmail.com>
2024-04-24 23:14:53 +0300

    Updating divmod.

--- divmod.go
+++ divmod.go
@@ -1,6 +1,12 @@
 package piscine
 
+/* Function that divides a and b and stores the result
+in the int pointed by div and the remainder in the int
+pointed by mod */
 func DivMod(a int, b int, div *int, mod *int) {
-	*div = (a / b)
-	*mod = (a % b)
+	//Declaring the variables
+	if b != 0 {
+		*div = a / b
+		*mod = a % b
+	}
 }
