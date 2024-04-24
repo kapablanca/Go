310ccef17dda9564e72110bddd206da8ac65c098
stath <efstathiosgkazanakis@protonmail.com>
2024-04-24 23:06:12 +0300

    Updating ultimatepointer.

--- ultimatepointone.go
+++ ultimatepointone.go
@@ -1,5 +1,8 @@
 package piscine
 
+/* Function that takes as argument a pointer
+ to a pointer to a pointer to an integer
+and gives it the value of 1 */
 func UltimatePointOne(n ***int) {
 	***n = 1
 }
