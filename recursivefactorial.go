257650429e9c95754a3c4344bc9969d47d35c6a7
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-25 13:53:36 +0300

    Updating recursivefactorial.

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -5,15 +5,15 @@ Function that returns the factorial of the int
 passed as parameter. Errors will return 0
 */
 func RecursiveFactorial(nb int) int {
-	var factorial int
 	// Check if negative number
 	if nb < 0 {
 		return 0
 	}
 	// Factorial of zero is 1
 	if nb == 0 {
-		factorial = 1
+		return 1
 	}
-	factorial *= RecursiveFactorial(nb - 1)
-	return factorial
+	// Return the number times the factorial of the previous number
+	// n! = n * (n-1)!
+	return nb * RecursiveFactorial(nb-1)
 }
