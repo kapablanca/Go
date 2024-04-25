1115f973ea23c03a4d188eb76b0c24690f6b76d4
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-25 14:03:07 +0300

    Updating iterativepower.

--- iterativepower.go
+++ iterativepower.go
@@ -1,15 +1,25 @@
 package piscine
 
+/*
+	Function that returns the value of nb to the power of power.
+
+Negative powers will return 0. Overflows do not have to be dealt with
+*/
 func IterativePower(nb int, power int) int {
+	// Check if power is negative
 	if power < 0 {
 		return 0
 	}
+
+	number := 1
+	// Number to the power of 0 is 1
 	if power == 0 {
-		return 1
+		return number
 	}
-	result := 1
-	for i := 1; i < power+1; i++ {
-		result *= nb
+
+	// Calculating  the result
+	for i := 0; i < power; i++ {
+		number *= nb
 	}
-	return result
+	return number
 }
