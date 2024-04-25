15b8661518d1d8b2ad2d341ae30fd3fb3971a729
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-25 14:16:05 +0300

    Updating fibonacci.

--- fibonacci.go
+++ fibonacci.go
@@ -1,12 +1,23 @@
 package piscine
 
+/*
+Function that recursively calculates the value
+
+at the position index in the fibonacci sequence.
+
+A negative index will return -1
+*/
 func Fibonacci(index int) int {
+	// Checking if index is negative
 	if index < 0 {
 		return -1
-	} else if index == 0 {
+	}
+
+	if index == 0 {
 		return 0
-	} else if index == 1 {
+	}
+	if index == 1 {
 		return 1
 	}
-	return Fibonacci(index-2) + Fibonacci(index-1)
+	return Fibonacci(index-1) + Fibonacci(index-2)
 }
