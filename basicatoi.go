d0eee7f6192410fa94a7f999a04527dc483578e6
stath <efstathiosgkazanakis@protonmail.com>
2024-04-25 09:45:20 +0300

    Updating basicatoi.

--- basicatoi.go
+++ basicatoi.go
@@ -7,6 +7,7 @@ func BasicAtoi(s string) int {
 	number := 0
 	digit := 0
 
+	// Turning each rune into an int and adding it to the number
 	for _, char := range s {
 		digit = int(char - '0')
 		number = 10*number + digit
