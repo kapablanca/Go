dcfc3c03dced3b5731ec5798c0c7d93afff8f4d3
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-25 14:52:01 +0300

    Updating sqrt.

--- sqrt.go
+++ sqrt.go
@@ -1,14 +1,21 @@
 package piscine
 
+/*
+Function that returns the square root of the int passed as parameter,
+
+if that square root is a whole number. Otherwise it returns 0
+*/
 func Sqrt(nb int) int {
+	// Check if number is greater than zero
 	if nb <= 0 {
 		return 0
 	}
-	result := 0
-	for i := 1; i*i <= nb; i++ {
-		if i*i == nb {
-			result = i
+	// Finding the sqaure root by checking if a number
+	// squared is equal to the nb
+	for root := 1; root*root <= nb; root++ {
+		if root*root == nb {
+			return root
 		}
 	}
-	return result
+	return 0
 }
