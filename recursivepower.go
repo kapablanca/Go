31a562490eae616d67430f258e372323b0b3fd04
Stathis Kazanakis <efstathiosgkazanakis@protonmail.com>
2024-04-25 14:09:22 +0300

    Updating recursivepower.

--- recursivepower.go
+++ recursivepower.go
@@ -1,9 +1,19 @@
 package piscine
 
+/*
+Function that recursively calculates the value of nb to
+
+the power of power. Negative powers will return 0. Overflows
+
+do not have to be dealt with.
+*/
 func RecursivePower(nb int, power int) int {
+	// Checking if the power is negative
 	if power < 0 {
 		return 0
-	} else if power == 0 {
+	}
+
+	if power == 0 {
 		return 1
 	}
 	return nb * RecursivePower(nb, power-1)
