7119084b9d98b82fb0775b1828ccd4a5dafe9888
stath <efstathiosgkazanakis@protonmail.com>
2024-04-25 09:42:00 +0300

    Updating basicatoi.

--- basicatoi.go
+++ basicatoi.go
@@ -1,13 +1,15 @@
 package piscine
 
+/* Function that transforms a number defined as string into an integer.
+For this exercise I take into account only valid strings and don't take into
+account the handling of signs */
 func BasicAtoi(s string) int {
-	var number int = 0
+	number := 0
+	digit := 0
 
-	for _, num := range s {
-
-		var digit rune = num - 48
-		number = number*10 + int(digit)
+	for _, char := range s {
+		digit = int(char - '0')
+		number = 10*number + digit
 	}
-
 	return number
 }
