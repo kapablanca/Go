c3e64e5261c9330adf4cfde2c6b81e88e654ca08
stath <efstathiosgkazanakis@protonmail.com>
2024-04-28 19:29:00 +0300

    Updating nrune

--- nrune.go
+++ nrune.go
@@ -1,12 +1,12 @@
 package piscine
 
-/* 
+/*
 Function that returns the n-th rune of a string.
 If not possible, it returns 0
- */
+*/
 func NRune(s string, n int) rune {
 	for index, char := range s {
-		if index == n - 1 {
+		if index == n-1 {
 			return char
 		}
 	}
