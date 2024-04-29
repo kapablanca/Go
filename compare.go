7b9fdbddabe7d65c706b8707e43e33d8cdcd3d24
stath <efstathiosgkazanakis@protonmail.com>
2024-04-29 12:34:55 +0300

    Updating nrune and compare

--- compare.go
+++ compare.go
@@ -1,7 +1,14 @@
 package piscine
 
-/* 
-Function that returns an integer comparing  */
+/*
+Function that returns an integer comparing
+*/
 func Compare(a, b string) int {
-
+	if a == b {
+		return 0
+	} else if a > b {
+		return 1
+	}
+	return -1
+	
 }
--- nrune.go
+++ nrune.go
@@ -1,7 +1,9 @@
 package piscine
 
-/* Function that returns the n-th rune of a string.
-If not possible, it returns 0 */
+/*
+Function that returns the n-th rune of a string.
+If not possible, it returns 0
+*/
 func NRune(s string, n int) rune {
 	for index, char := range s {
 		if index == n-1 {
