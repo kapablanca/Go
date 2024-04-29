033becd17d2a6e1dcb79daebb2918b30b3f2d6d4
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-04-29 18:06:24 +0300

    Updating

--- printcombn.go
+++ printcombn.go
@@ -2,9 +2,9 @@ package piscine
 
 // import "github.com/01-edu/z01"
 
-/*Function that prints all possible combinations
- of n different digits in ascending order.
- n will be defined as : 0 < n < 10  */
- func PrintCombN(n int) {
-	
- }
+// Function that prints all possible combinations
+//  of n different digits in ascending order.
+// n will be defined as : 0 < n < 10
+func PrintCombN(n int) {
+
+}
