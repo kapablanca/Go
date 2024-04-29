09e41baf6df2c225ff9c411ce3c8b2514ea6a6b0
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-04-29 14:03:11 +0300

    Updating nrune.

--- printcombn.go
+++ printcombn.go
@@ -1,36 +1,10 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+// import "github.com/01-edu/z01"
 
-func PrintCombN(n int) {
-	generateCombinations("", 0, n)
-}
-
-func generateCombinations(prefix string, start, n int) {
-	if len(prefix) == n {
-		for _, r := range prefix {
-			z01.PrintRune(rune(r))
-		}
-		// Determine if this is the last combination to decide whether to print a comma and space
-		if prefix < "123456789"[9-n:] {
-			z01.PrintRune(',')
-			z01.PrintRune(' ')
-		} else {
-			z01.PrintRune('\n') // Print a newline at the end
-		}
-		return
-	}
-
-	for i := start; i <= 9; i++ {
-		newPrefix := prefix + intToString(i) // Use the custom int to string conversion
-		generateCombinations(newPrefix, i+1, n)
-	}
-}
-
-// intToString converts an integer to its string representation.
-// This version directly constructs the string expected for single digits, leveraging rune conversion.
-func intToString(num int) string {
-	return string('0' + rune(num))
-}
+/*Function that prints all possible combinations
+ of n different digits in ascending order.
+ n will be defined as : 0 < n < 10  */
+ func PrintCombN(n int) {
+	
+ }
