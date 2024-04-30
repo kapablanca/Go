59d84a2965151a42798bc9a467005ab5a07bdd50
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-04-30 12:38:59 +0300

    Updating isalpha.

--- isalpha.go
+++ isalpha.go
@@ -1,8 +1,10 @@
 package piscine
 
+// Function that returns true if the string passed as the parameter only
+// contains alphanumerical characters or is empty, else returns false
 func IsAlpha(s string) bool {
-	for _, letter := range s {
-		if letter > 'z' || (letter < 'a' && letter > 'Z' || (letter < 'A' && letter > '9' || letter < '0' && letter != ' ')) {
+	for _, char := range s {
+		if !IsUpper(string(char)) && !IsLower(string(char)) && !isNumber(char) {
 			return false
 		}
 	}
