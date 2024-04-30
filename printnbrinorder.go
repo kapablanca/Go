d2da77df93d3d9c42c0522b8cdc98f40c9bd0b05
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-04-30 14:11:14 +0300

    Updating printnbrinorder.

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -2,27 +2,39 @@ package piscine
 
 import "github.com/01-edu/z01"
 
+// Function that prints the digits of an int passed in parameter
+// in ascending order. All possible values of type int have to go through,
+// excluding negative numbers. Conversion to int64 is not allowed.
 func PrintNbrInOrder(n int) {
 	if n < 0 {
 		return
 	}
 
-	var count_digits [10]int
-	digit := 0
-
 	if n == 0 {
-		count_digits[0] = 1
-	} else {
-		for n > 0 {
-			digit = n % 10
-			count_digits[digit] += 1
-			n /= 10
-		}
-	}
+		z01.PrintRune('0')
 
-	for i := 0; i < len(count_digits); i++ {
-		for j := 0; j < count_digits[i]; j++ {
-			z01.PrintRune(rune('0' + i))
+	} else {
+		digits := []int{}
+		mod := -1
+		// putting the digits of n in the slice digits
+		for n != 0 {
+			mod = n % 10
+			n = n / 10
+			digits = append(digits, mod)
+		}
+
+		// Reorder the elements of digits in ascending order
+		for i := 0; i < len(digits)-1; i++ {
+			for j := i; j < len(digits)-1; j++ {
+				if digits[j+1] < digits[j] {
+					digits[j], digits[j+1] = digits[j+1], digits[j]
+				}
+			}
+		}
+
+		// Printing the number
+		for _, digit := range digits {
+			z01.PrintRune(rune(digit) + '0')
 		}
 	}
 }
