58af0a4df66a6ab4d0d621e84128f8456fac9aaf
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-03 00:41:39 +0300

    Updating capitalize.

--- capitalize.go
+++ capitalize.go
@@ -1,31 +1,31 @@
 package piscine
 
+// Function that capitalizes the first letter of
+// each word and lowercases the rest. A word is a
+// sequence of alphanumeric characters.
 func Capitalize(s string) string {
-	var answer string
-	var last_letter string
-	for _, letter := range s {
-		letter := string(letter)
+	capitalized := ""
+	word := ""
 
-		if !IsAlpha(letter) || IsNumeric(letter) || letter == " " {
-			answer += letter
-		} else {
-			if answer == "" {
-				if IsNumeric(letter) {
-					answer += letter
-				} else {
-					answer += ToUpper(letter)
-				}
-			} else {
-				last_letter = string(LastRune(answer))
-				if IsNumeric(last_letter) || IsUpper(last_letter) || IsLower(last_letter) {
-					answer += ToLower(letter)
-				} else {
-					answer += ToUpper(letter)
-				}
-
+	for _, char := range s {
+		substring := string(char)
+		// Check if the substring is alphanumeric char
+		if IsAlpha(substring) {
+			// Capitalize the first letter of word if needed
+			if word == "" && IsLower(substring) {
+				substring = ToUpper(substring)
 			}
+			// Lowercase the letter if needed
+			if word != "" && IsUpper(substring) {
+				substring = ToLower(substring)
+			}
+			word += substring
+			//Append the non alpha char and clear the word variable
+		} else {
+			capitalized += word + substring
+			word = ""
 		}
-
 	}
-	return answer
+	capitalized += word
+	return capitalized
 }
