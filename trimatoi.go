728aa107ced3bff460c5b7ff2337e9ff5595c6c1
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-02 21:47:56 +0300

    Updating trimatoi.

--- trimatoi.go
+++ trimatoi.go
@@ -1,15 +1,24 @@
 package piscine
 
+// Function that transforms numbers within a string into an int.
+// If the - sign is encountered before any number, it should determine
+// the sign of the returned int. In case of invalid input, the function
+// should return 0.
 func TrimAtoi(s string) int {
-	num := 0
+	number := 0
+	isValid := false
 	sign := 1
 
 	for _, char := range s {
-		if char == '-' && num == 0 {
+		// negative sign if it hasn't found a number yet
+		if char == '-' && !isValid {
 			sign = -1
-		} else if char >= '0' && char <= '9' {
-			num = num*10 + int(char-'0')
+		}
+		if IsNumeric(string(char)) {
+			isValid = true
+			number = number*10 + int(char-'0')
 		}
 	}
-	return num * sign
+
+	return sign * number
 }
