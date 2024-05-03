e0862812dc1e6a37ebe00f663346f0d50a51070d
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-03 16:13:24 +0300

    Updating sortparams/.

--- sortparams/main.go
+++ sortparams/main.go
@@ -6,21 +6,30 @@ import (
 	"github.com/01-edu/z01"
 )
 
+func printString(s string) {
+	for _, char := range s {
+		z01.PrintRune(char)
+	}
+}
+
+// Program that prints the arguments received in the command line
+// in ASCII order
 func main() {
 	arguments := os.Args[1:]
+	if len(arguments) < 1 {
+		return
+	}
 
-	for i := 0; i < len(arguments); i++ {
-		for j := 0; j < len(arguments)-1; j++ {
-			if arguments[j] > arguments[j+1] {
-				arguments[j], arguments[j+1] = arguments[j+1], arguments[j]
+	for range arguments {
+		for i := 0; i < len(arguments)-1; i++ {
+			if arguments[i+1] < arguments[i] {
+				arguments[i], arguments[i+1] = arguments[i+1], arguments[i]
 			}
 		}
 	}
 
-	for _, word := range arguments {
-		for _, character := range word {
-			z01.PrintRune(character)
-		}
+	for _, arg := range arguments {
+		printString(arg)
 		z01.PrintRune('\n')
 	}
 }
