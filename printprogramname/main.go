709a05da652129070351fa04e7fa8fba84a6547b
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-03 15:40:05 +0300

    Updating programname/.

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,38 +1,3 @@
-// package main
-
-// import (
-// 	"os"
-
-// 	"github.com/01-edu/z01"
-// )
-
-// // Program that prints the name of the program
-// func main() {
-// 	arg := os.Args[0]
-// 	lastSlash := -1
-// 	hasExtension := false
-// 	lastDot := -1
-// 	// Extracting the program name from the filepath
-// 	for index, char := range arg {
-// 		if char == '\\' {
-// 			lastSlash = index
-// 		}
-// 		if char == '.' {
-// 			lastDot = index
-// 			hasExtension = true
-// 		}
-// 	}
-// 	if hasExtension {
-// 		arg = arg[lastSlash+1 : lastDot]
-// 	} else {
-// 		arg = arg[lastSlash+1:]
-// 	}
-// 	for _, char := range arg {
-// 		z01.PrintRune(char)
-// 	}
-// 	z01.PrintRune('\n')
-// }
-
 package main
 
 import (
@@ -41,6 +6,7 @@ import (
 	"github.com/01-edu/z01"
 )
 
+// Program that prints the name of the program
 func main() {
 	arguments := os.Args[0][2:]
 	for _, letter := range arguments {
