5e05d3e151903306f3ae29a8b06895f821f51839
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-03 15:50:45 +0300

    Updating revparams/.

--- revparams/main.go
+++ revparams/main.go
@@ -6,16 +6,22 @@ import (
 	"github.com/01-edu/z01"
 )
 
+// Function that prints a string
+func printString(s string) {
+	for _, char := range s {
+		z01.PrintRune(char)
+	}
+}
+
+// Program that prints the arguments received in reverse order
 func main() {
-	arguments := os.Args
+	arguments := os.Args[1:]
+	if len(arguments) < 1 {
+		return
+	}
 
-	if len(arguments) > 1 {
-		for i := len(arguments) - 1; i > 0; i-- {
-
-			for _, letter := range arguments[i] {
-				z01.PrintRune(letter)
-			}
-			z01.PrintRune('\n')
-		}
+	for i := len(arguments) - 1; i >= 0; i-- {
+		printString(arguments[i])
+		z01.PrintRune('\n')
 	}
 }
