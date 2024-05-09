830dac07e643161a63a00e07d06dace98272bab2
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-10 00:46:49 +0300

    Not complete update of flags

--- flags/main.go
+++ flags/main.go
@@ -5,118 +5,104 @@ import (
 	"os"
 )
 
+// Program that can take --insert (or -i), --order (or -o) and
+//  a string as arguments. This program should:
+// -Insert the string given to the --insert (or -i), into the argument string, if given.
+// -If the flag --order (or -o) is given, order the string argument (in ASCII order).
+// -If there are no arguments or if the flag --help (or -h) is given, the options should be printed as in the example.
+// The short flag will have two spaces before the (-).
+// The explanation of the flag will have a tab followed by a space before the beginning of the sentence (This flag...)
+
+// Prints the option of a flag
+func printOption(flag, option string) {
+	shortFlag := flag[1:3]
+	fmt.Println(flag)
+	fmt.Print("  " + shortFlag)
+	fmt.Println("\t" + " " + option)
+}
+
+// Checks for help flag
+func hasFlag(slice []string, flag string) (int, bool) {
+	shortFlag := flag[1:3]
+	for index, arg := range slice {
+		if arg[:len(flag)] == flag || arg[:len(shortFlag)] == shortFlag {
+			return index, true
+		}
+	}
+	return -1, false
+}
+
+// Order a string argument in ASCII order
+func orderString(s string) string {
+	stringSlice := []rune(s)
+
+	for range stringSlice {
+		for i := 0; i < len(stringSlice)-1; i++ {
+			if stringSlice[i+1] < stringSlice[i] {
+				stringSlice[i], stringSlice[i+1] = stringSlice[i+1], stringSlice[i]
+			}
+		}
+	}
+	return string(stringSlice)
+}
+
+// Insert a string to a given string
+func insertString(flagString, s string) string {
+	indexString := 0
+	for index, char := range flagString {
+		if char == '=' {
+			indexString = index + 1
+		}
+	}
+	return s + flagString[indexString:]
+}
+
 func main() {
 	arguments := os.Args[1:]
-
-	// Stating the option strings
-	insert_string := "--insert\n" + "  -i"
-	order_string := "--order\n" + "  -o"
-	insert_description := "This flag inserts the string into the string passed as argument."
-	order_description := "This flag will behave like a boolean, if it is called it will order the argument."
-
-	// Stating the option keywords
-	// long_insert := "--insert"
-	// short_insert := "-i"
-	long_order := "--order"
-	short_order := "-o"
-	// Stating the variables in use
-	is_order := false
-	is_insert := false
-	index_insert := 0
-	index_string := 0
-	var answer string
-
-	// Checking if the help option was selected
-	if len(arguments) == 0 || arguments[0] == "--help" || arguments[0] == "-h" {
-
-		fmt.Println(insert_string)
-		fmt.Println("\t " + insert_description)
-		fmt.Println(order_string)
-		fmt.Println("\t " + order_description)
-
-	} else {
-		// Print the string argument
-		if len(arguments) == 1 {
-
-			answer = arguments[0]
-			fmt.Println(answer)
-
-		} else {
-			// Check for the flags and where they are
-			for i, arg := range arguments {
-
-				if IsInsert(arg) {
-					is_insert = true
-					index_insert = i
-				}
-
-				if arg == long_order || arg == short_order {
-					is_order = true
-				}
-				// Saving the place of the string argument
-				index_string = i
-			}
-		}
-		// Checking insert instruction
-		if is_insert {
-			answer = InsertArg(string(arguments[index_insert]), string(arguments[index_string]))
-		} else {
-			// Inserting just the sting argument to return
-			answer = arguments[index_string]
-		}
-		// Checking order instruction
-		if is_order {
-			answer = OrderArg(answer)
-		}
-		// Printing the final answer
-		fmt.Println(answer)
-	}
-}
-
-// Inserting function
-func InsertArg(arg string, extra string) string {
-	var answer string
-	index := 0
-	// Checking the index of where the equal sign is
-	for i, char := range arg {
-		if char == '=' {
-			index = i
-		}
-	}
-	// Extracting the string argument to be inserted
-	arg = arg[index+1:]
-
-	answer = extra + arg
-
-	return answer
-}
-
-// Ordering function
-func OrderArg(arg string) string {
-	var answer string
-	arguments := []rune(arg)
-	// Ordering algorithm
-	for i := 0; i < len(arguments); i++ {
-		for j := 0; j < len(arguments)-1; j++ {
-			if arguments[j] > arguments[j+1] {
-				arguments[j], arguments[j+1] = arguments[j+1], arguments[j]
-			}
-		}
-	}
-	// Converting []rune to string
-	for _, char := range arguments {
-		answer += string(char)
+	help := false
+	insert := false
+	order := false
+	insertIn := -1
+	orderIn := -1
+	// Check if there are no arguments
+	if len(arguments) < 1 {
+		help = true
 	}
 
-	return answer
-}
+	// Check if there are more arguments
+	if len(arguments) > 3 {
+		return
+	}
 
-// Checking if there is insert inside an argument
-func IsInsert(s string) bool {
-	for _, char := range s {
-		if char == '=' {
-			return true
+	// Declaring the flag variables and options
+	flagInsert := "--insert"
+	optionI := "This flag inserts the string into the string passed as argument."
+	flagOrder := "--order"
+	optionO := "This flag will behave like a boolean, if it is called it will order the argument."
+	flagHelp := "--help"
+	flags := []string{flagInsert, flagOrder}
+	options := []string{optionI, optionO}
+
+	// Checking for help flag in arguments
+	if !help {
+		_, help = hasFlag(arguments, flagHelp)
+	}
+
+	// Printing options for each flag
+	if help {
+		for index, flag := range flags {
+			printOption(flag, options[index])
+		}
+		return
+	}
+
+	insertIn, insert = hasFlag(arguments, flagInsert)
+	orderIn, order = hasFlag(arguments, flagOrder)
+
+	for index, arg := range arguments {
+		if insert && order {
+
 		}
 	}
-	return false
+
 }
