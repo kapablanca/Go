f99e7c6e97172f81e5600c78a1d2c1bd95bef231
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-18 17:38:54 +0300

    Update appendrange.go

--- appendrange.go
+++ appendrange.go
@@ -1,13 +1,16 @@
 package piscine
 
+// Function that takes an inmin and and an int max as parameters.
+// The function should return a slice of int s with all the values
+// between the min and max. Min is included, max is excluded.
+// If min >= max, a nil slice is returned. Make is not allowed.
 func AppendRange(min, max int) []int {
-	var series []int
-
+	var s []int
 	if min >= max {
-		return series
+		return s
 	}
 	for i := min; i < max; i++ {
-		series = append(series, i)
+		s = append(s, i)
 	}
-	return series
+	return s
 }
