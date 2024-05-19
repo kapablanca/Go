afd3cee9ea7a5ef6829f1c761e9b409d16dee698
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-19 14:04:22 +0300

    Update makerange.

--- makerange.go
+++ makerange.go
@@ -1,17 +1,18 @@
 package piscine
 
+// Function that takes an int min and int max as parameters.
+// The function must return a slice of int s with all the values
+// between min and max. Min is included, max is excluded.
+// If min >= max, a nil slice is returned. Append is not allowed for this exercise.
 func MakeRange(min, max int) []int {
-	var series []int
-
 	if min >= max {
-		return series
-	} else {
-		size := max - min
-		series = make([]int, size)
-
-		for i := 0; i < size; i++ {
-			series[i] = i + min
-		}
+		var s []int
+		return s
 	}
-	return series
+	s := make([]int, max-min)
+	for index := range s {
+		s[index] = min
+		min++
+	}
+	return s
 }
