62d98216464ebfe16bc3e65e6dba8d6d96313d31
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-19 14:57:04 +0300

    Updating concatparams.

--- concatparams.go
+++ concatparams.go
@@ -1,5 +1,8 @@
 package piscine
 
+// Function that takes the arguments received in parameters
+// and returns them as a string. The string is the result of
+// all the arguments concatenated with a newline between.
 func ConcatParams(args []string) string {
 	return Join(args, "\n")
 }
