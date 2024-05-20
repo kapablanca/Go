10e5d47139f5d0afd5c9b5418720606ccb141031
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-21 00:47:46 +0300

    Updating printwordstables.

--- printwordstables.go
+++ printwordstables.go
@@ -5,13 +5,10 @@ import "github.com/01-edu/z01"
 // Function that receives a string slice and prints
 // each element of the slice in a seperate line
 func PrintWordsTables(a []string) {
-	for i, word := range a {
+	for _, word := range a {
 		for _, char := range word {
 			z01.PrintRune(char)
 		}
-		// Don't print new line for the last word
-		if i != len(a)-1 {
-			z01.PrintRune('\n')
-		}
+		z01.PrintRune('\n')
 	}
 }
