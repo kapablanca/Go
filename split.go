e5c30da0ebc452bbcc17522741c9ba4f1a549bc0
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-22 11:11:02 +0300

    Updating split.

--- split.go
+++ split.go
@@ -1,26 +1,27 @@
 package piscine
 
+// Function that receives a string and a separator and returns a slice of strings
+// that results of splitting the string s by the separator sep
 func Split(s, sep string) []string {
 	var words []string
+	var word []rune
+	// Extending the string to stop the iteration in the end
+	s += sep
 
-	for i := 0; i < len(s)-len(sep); {
-
-		j := len(sep)
-
-		if s[i:i+j] == sep {
-
-			if i != 0 {
-				words = append(words, s[:i])
+	for i := 0; i <= len(s)-len(sep); i++ {
+		if s[i:i+len(sep)] == sep {
+			// Make the next iteration pass the seperator by changing index
+			i += len(sep) - 1
+			// Go to the next iter if there is no word to append
+			if word == nil {
+				continue
 			}
-			s = s[i+j:]
-			i = 0
+			// Extract the word and append it to the slice words
+			words = append(words, string(word))
+			word = nil
 		} else {
-			i++
+			word = append(word, rune(s[i]))
 		}
-
-	}
-	if len(s) > 0 {
-		words = append(words, s)
 	}
 	return words
 }
