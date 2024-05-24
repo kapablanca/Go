7b55bc8e16f06acc4b4b8e2c106f00f01b121c23
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-24 17:17:07 +0300

    Updating boolean/

--- boolean/main.go
+++ boolean/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/01-edu/z01"
 )
 
+// Making changes to the code to make it work
 func printStr(s string) {
 	for _, r := range s {
 		z01.PrintRune(r)
@@ -13,29 +14,21 @@ func printStr(s string) {
 	z01.PrintRune('\n')
 }
 
-func even(nbr int) int {
+func isEven(nbr int) bool {
 	if nbr%2 == 0 {
-		return 1
-	}
-	return 0
-}
-
-func isEven(nbr int) int {
-	if even(nbr) == 1 {
-		return 1
+		return true
 	} else {
-		return 0
+		return false
 	}
 }
 
 func main() {
-	EvenMsg := "I have an even number of arguments"
 	OddMsg := "I have an odd number of arguments"
-
+	EvenMsg := "I have an even number of arguments"
 	arguments := os.Args[1:]
 	lengthOfArg := len(arguments)
 
-	if isEven(lengthOfArg) == 1 {
+	if isEven(lengthOfArg) {
 		printStr(EvenMsg)
 	} else {
 		printStr(OddMsg)
