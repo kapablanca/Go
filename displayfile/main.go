98aa32a09e0c426e08b31d7b65ff5118e7c769dd
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-24 19:24:01 +0300

    Updating displayfile/

--- displayfile/main.go
+++ displayfile/main.go
@@ -2,25 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
+// Program that displays on the standard output the content of a file,
+//
+//	give as an argument
 func main() {
-	arguments := os.Args[1:]
+	argument := os.Args[1:]
 
-	if len(arguments) < 1 {
-		fmt.Println("File name missing")
-	} else if len(arguments) > 1 {
-		fmt.Println("Too many arguments")
-	} else {
-
-		file, _ := os.Open(string(arguments[0]))
-
-		arr := make([]byte, 15)
-		file.Read(arr)
-		fmt.Print(string(arr))
-
-		file.Close()
-
+	if len(argument) < 1 {
+		fmt.Print("File name missing")
+		return
 	}
+	if len(argument) > 1 {
+		fmt.Print("Too many arguments")
+		return
+	}
+	// Opening the file
+	file, err := os.Open(argument[0])
+	if err != nil {
+		return
+	}
+	// Make sure the file will close eventually
+	defer file.Close()
+	// Reading the contents of the file
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return
+	}
+
+	text := string(content)
+	fmt.Print(text)
 }
