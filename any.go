14f0244fffb8106f145b94a18559a6252c187912
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-05-25 19:03:51 +0300

    Updating any.

--- any.go
+++ any.go
@@ -4,9 +4,9 @@ package piscine
 //  when that string slice is passed through an f function,
 // at least one elements returns true
 
-func Any(f func(string) bool, a[]string) bool {
-	for _, elem := ramge a {
-		if f(elem){
+func Any(f func(string) bool, a []string) bool {
+	for _, elem := range a {
+		if f(elem) {
 			return true
 		}
 	}
