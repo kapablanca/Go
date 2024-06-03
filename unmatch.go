62141ecffa15b7cb9f097e4635c6bd74bad0f9d5
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-03 10:54:46 +0300

    Updating umatch.

--- unmatch.go
+++ unmatch.go
@@ -1,6 +1,6 @@
 package piscine
 
-// Function that returns the element of the slice that does not have a
+// Function that returns the first element of the slice that does not have a
 //  correspondet pair. If all the numbers have a correspondent pair, it should return -1
 func Unmatch(a []int) int {
 	// Create a map to count how many times a number appears
