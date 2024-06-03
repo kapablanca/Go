0380c5cc01011504075d59dc75c652c29ad5f149
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-03 23:04:22 +0300

    Updating stringtointslice.

--- stringtointslice.go
+++ stringtointslice.go
@@ -2,7 +2,7 @@ package piscine
 
 // Function that takes a string and returns the corresponding int slice
 func StringToIntSlice(str string) []int {
-	intSlice := []int{}
+	var intSlice []int
 
 	for _, char := range str {
 		intSlice = append(intSlice, int(char))
