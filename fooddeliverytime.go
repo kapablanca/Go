102311457b309b3edc4461b312c1a87f6fa8c605
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-03 11:45:26 +0300

    Updating fooddeliverytime.

--- fooddeliverytime.go
+++ fooddeliverytime.go
@@ -1,27 +1,34 @@
 package piscine
 
 type food struct {
-	preptime int
 	name     string
+	preptime int
 }
 
+// Function to create menu item
+func MenuItem(name string, time int) *food {
+	i := food{name: name, preptime: time}
+	return &i
+}
+
+// Given the following menu with the coresponding times that each item takes to cook
+// (burger takes 15 min, chips takes 10 min and nuggets takes 12 min), return the time
+//
+//	that each order item takes to be prepared and the toteal amount of time for the order
+//
+// to be ready assuming the items are cooked one after the other. If any of the order items
+//
+//	don't exist in the menu above return the error message 404
 func FoodDeliveryTime(order string) int {
-	// Declaring the menu items and the preperation time of each item
-	burger := food{}
-	chips := food{}
-	nuggets := food{}
+	// Creating given menu items
+	burger := MenuItem("burger", 15)
+	chips := MenuItem("chips", 10)
+	nuggets := MenuItem("nuggets", 12)
 
-	burger.preptime = 15
-	burger.name = "burger"
-	chips.preptime = 10
-	chips.name = "chips"
-	nuggets.preptime = 12
-	nuggets.name = "nuggets"
-
-	menu := []food{burger, chips, nuggets}
+	menu := []*food{burger, chips, nuggets}
 
 	for _, item := range menu {
-		if order == item.name {
+		if item.name == order {
 			return item.preptime
 		}
 	}
