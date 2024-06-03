31f1ba8c3b0b1f00febe19d492fc9f275e1ced1a
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-03 10:40:48 +0300

    Updating unmatch.

--- unmatch.go
+++ unmatch.go
@@ -1,15 +1,21 @@
 package piscine
 
+import "fmt"
+
+// Function that returns the element of the slice that does not have a
+//  correspondet pair. If all the numbers have a correspondent pair, it should return -1
 func Unmatch(a []int) int {
-	counts := make(map[int]int)
-
-	for _, nbr := range a {
-		counts[nbr]++
+	// Create a map to count how many times a number appears
+	numbersFreq := make(map[int]int)
+	// Counting numbers
+	for _, i := range a {
+		numbersFreq[i]++
 	}
-
-	for _, nbr := range a {
-		if counts[nbr]%2 != 0 {
-			return nbr
+	fmt.Print(numbersFreq)
+	// Finding pairs
+	for key, value := range numbersFreq {
+		if value%2 != 0 {
+			return key
 		}
 	}
 	return -1
