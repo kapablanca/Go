06dc17dcafc77e50d90149df6dd35d93ac7305b7
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-03 10:43:07 +0300

    Updating unmatch.

--- unmatch.go
+++ unmatch.go
@@ -1,7 +1,5 @@
 package piscine
 
-import "fmt"
-
 // Function that returns the element of the slice that does not have a
 //  correspondet pair. If all the numbers have a correspondent pair, it should return -1
 func Unmatch(a []int) int {
@@ -11,7 +9,6 @@ func Unmatch(a []int) int {
 	for _, i := range a {
 		numbersFreq[i]++
 	}
-	fmt.Print(numbersFreq)
 	// Finding pairs
 	for key, value := range numbersFreq {
 		if value%2 != 0 {
