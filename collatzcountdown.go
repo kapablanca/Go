b9d30a6c45345954fe25bc6aac0e80f8c49da95e
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-03 12:40:10 +0300

    Updating collatzcountdown.

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,20 +1,27 @@
 package piscine
 
+// Function that returns the number of steps necessary to reach 1
+// using the collatz countdown. It must return -1 if start is
+// equal to 0 or negative.
 func CollatzCountdown(start int) int {
+	steps := 0
+	// Check if start is non positive
 	if start <= 0 {
 		return -1
 	}
-	count := 0
 	if start == 1 {
-		count = 1
+		steps++
+		return steps
 	}
 	for start != 1 {
+		// Start is even
 		if start%2 == 0 {
 			start /= 2
+			// Start is odd
 		} else {
 			start = 3*start + 1
 		}
-		count++
+		steps++
 	}
-	return count
+	return steps
 }
