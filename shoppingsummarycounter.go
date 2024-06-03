aa72e080763c684f44b7a4f4d8c42a2294d6ac97
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-03 13:24:17 +0300

    Updating shoppingsummarycounter.

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,39 +1,13 @@
 package piscine
 
-func SplitOneSpace(str string) []string {
-	var words []string
-	currentWord := ""
-
-	// Process each character in the string
-	for i := 0; i < len(str); i++ {
-		if str[i] == ' ' {
-			// Check if currentWord has accumulated any characters before adding it to words
-			// if currentWord != "" {
-			words = append(words, currentWord)
-			currentWord = "" // Reset currentWord after adding it to words
-			// }
-			// Continue to next iteration to skip additional space processing
-			continue
-		}
-		// Add non-space characters to currentWord
-		currentWord += string(str[i])
-	}
-
-	// Add the last accumulated word to words if not empty
-
-	words = append(words, currentWord)
-
-	return words
-}
-
+// Function that counts the total amount of times each item appears in a string
+// and returns a summary including the item and its number of appearances as an int
 func ShoppingSummaryCounter(str string) map[string]int {
-	items_slice := SplitOneSpace(str)
-
-	shopping_list := make(map[string]int)
-
-	for _, item := range items_slice {
-		shopping_list[item]++
+	shoppingList := Split(str, " ")
+	summary := make(map[string]int)
+	// Count the times an item appears on the list
+	for _, item := range shoppingList {
+		summary[item]++
 	}
-
-	return shopping_list
+	return summary
 }
