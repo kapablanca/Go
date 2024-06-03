49e150199522ed7b920d65ed042b822433ab86c8
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-03 10:13:28 +0300

    Updating descendcomb.

--- descendcomb.go
+++ descendcomb.go
@@ -2,27 +2,30 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintTwoDigits(nbr int) {
-	digit1 := nbr / 10
-	digit2 := nbr % 10
-
-	z01.PrintRune(rune(digit1 + '0'))
-	z01.PrintRune(rune(digit2 + '0'))
-}
-
-func PrintSeperator() {
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-}
-
+// Function that prints in descending order and on a single line all
+//  possible combinations of two different two-digit numbers. These
+//  combinations are separated by a commma and a space
 func DescendComb() {
+	var digit1, digit2, digit3, digit4 rune
+
 	for i := 99; i > 0; i-- {
 		for j := i - 1; j >= 0; j-- {
-			PrintTwoDigits(i)
+			// First number digits
+			digit1 = rune(i/10) + '0'
+			digit2 = rune(i%10) + '0'
+			// Second number digits
+			digit3 = rune(j/10) + '0'
+			digit4 = rune(j%10) + '0'
+			// Printing numbers
+			z01.PrintRune(digit1)
+			z01.PrintRune(digit2)
 			z01.PrintRune(' ')
-			PrintTwoDigits(j)
-			if i != 1 {
-				PrintSeperator()
+			z01.PrintRune(digit3)
+			z01.PrintRune(digit4)
+			// Printing seperator
+			if i != 0 {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			}
 		}
 	}
