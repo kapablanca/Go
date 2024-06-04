8280ff6b533b649f0c61515cc7f9e3513d837974
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-04 12:36:50 +0300

    Updating enigma.

--- enigma.go
+++ enigma.go
@@ -1,9 +1,11 @@
 package piscine
 
+// Function that receives pointers as arguments and move its values
+// around to hide them. This function will put:
+// a into c, c into d,
+// d into b, b into a.
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	temp := ***a
-	***a = *b
-	*b = ****d
-	****d = *******c
-	*******c = temp
+	// Change all the values at the same time
+	*******c, ****d, ***a, *b = ***a, *******c, *b, ****d
+
 }
