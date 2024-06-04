1306503623407680932bf9b87865f3e815421166
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-04 12:52:22 +0300

    Updating descendappendrange.

--- descendappendrange.go
+++ descendappendrange.go
@@ -1,11 +1,16 @@
 package piscine
 
+// Function that returns a slice of int with all the values between
+//  the max and min. The max must be included and min must be excluded.
+// if max is inferior than or equal to min, return an empty slice.
+// make() is not allowed for this exercise
 func DescendAppendRange(max, min int) []int {
 	list := []int{}
-
+	// Check min, max
 	if max <= min {
 		return list
 	}
+	// Find the values between min-max
 	for i := max; i > min; i-- {
 		list = append(list, i)
 	}
