4c9ab673a6b7be15bd2a39a2faf34cd9cfe796ff
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-04 13:25:17 +0300

    First try.

---
+++ reversemenuindex.go
@@ -0,0 +1,9 @@
+package piscine
+
+// Function that takes a slice of strings(menu) and returns
+// another slice with the items in the correct order.
+// append() is not allowed for this exercise.
+func ReverseMenuIndex(menu []string) []string {
+	correctMenu := []string{"starters", "drinks", "mains", "desserts"}
+	return correctMenu
+}
