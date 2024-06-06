06f240fdef7a3f86ddf58ee337d01fd113659ee4
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-06 11:18:23 +0300

    Updating reversemenuindex.

--- reversemenuindex.go
+++ reversemenuindex.go
@@ -4,7 +4,6 @@ package piscine
 // another slice with the items in the correct order.
 // append() is not allowed for this exercise.
 func ReverseMenuIndex(menu []string) []string {
-
 	for i, j := 0, len(menu)-1; i < len(menu)/2; i, j = i+1, j-1 {
 		menu[i], menu[j] = menu[j], menu[i]
 	}
