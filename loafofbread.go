9e25d66cc192fa81485866bc7393dc0ac076cd25
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-08 20:13:55 +0300

    Updating loafofbread.

--- loafofbread.go
+++ loafofbread.go
@@ -1,31 +1,5 @@
 package piscine
 
-// Function that removes spaces from the string. If a space is in the place
-// where the character would be skipped from LoadOfBread(), the returned string
-// has a '6' in its place.
-func trim(s string) string {
-	countSpaces := 0
-	countChars := 0
-	newString := []rune{}
-	// Check each char of the string
-	for _, char := range s {
-		countChars += 1
-		// If the space would be skipped
-		if char == ' ' && (countChars-countSpaces)%6 == 0 {
-			newString = append(newString, '6')
-			countSpaces += 1
-			// If the space is in the middle of the string
-		} else if char == ' ' {
-			countSpaces += 1
-			continue
-			// Append any other char
-		} else {
-			newString = append(newString, char)
-		}
-	}
-	return string(newString)
-}
-
 // Function that takes a string and returns another one with words of
 // 5 characters and skips the next character followed by newline.
 // If there is space in the middle of a string it should ignore it and
@@ -42,29 +16,42 @@ func LoafOfBread(str string) string {
 		return "Invalid Output\n"
 	}
 
-	// Remove the middle spaces from string
-	str = trim(str)
 	// Declare variables
 	count := 0
 	word := []rune{}
-	sentence := []rune{}
+	result := []rune{}
+	var char rune
 	// Loop through the string to create new 5 char words
-	for _, char := range str {
-		count += 1
-		// If we reached the 5 chars to format a word
-		if count > 5 {
-			count = 0
-			word = append(word, ' ')
-			sentence = append(sentence, word...)
-			word = word[:0]
+	for i := 0; i < len(str); i++ {
+		char = rune(str[i])
+		// Skip the space
+		if char == ' ' {
 			continue
-		} else {
-			word = append(word, char)
+		}
+
+		// Append char to word
+		word = append(word, char)
+		count++
+		// If we have 5 character word
+		if count == 5 {
+			// Append word to result
+			result = append(result, word...)
+			// Append space to seperate words
+			result = append(result, ' ')
+			// Skip the next char(even if it's space)
+			i++
+			// Reset word count and word
+			count = 0
+			word = []rune{}
 		}
 	}
 	// Append the last word to the final string
 	if len(word) != 0 {
-		sentence = append(sentence, word...)
+		result = append(result, word...)
 	}
-	return string(sentence) + "\n"
+	// Remove trailing space if there is one
+	if result[len(result)-1] == ' ' {
+		result = result[:len(result)-1]
+	}
+	return string(result) + "\n"
 }
