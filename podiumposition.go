399e77a46a7d77fcebb13d5f054f6c0aa6071cf1
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-08 13:20:03 +0300

    Updating podiumposition.

--- podiumposition.go
+++ podiumposition.go
@@ -13,21 +13,14 @@ func hasChar(s string, r rune) bool {
 // Function that takes an unorderd slice of slices  of string and returns
 // the competitor's positions correctly.
 func PodiumPosition(podium [][]string) [][]string {
-	// rows := len(podium)
-	// cols := len(podium[0])
-	// Slice that holds the podium positions
-	positions := []rune{'1', '2', '3', '4'}
-	// Make an emptry 2d array with same dimensions as podium
-	// var newPodium [][]string
-	// for i := range podium {
-	// 	newPodium[i] = []string{""}
-	// }
+	var position rune
 	// Search the position on each string and put it in the correct order
-	for index, char := range positions {
+	for index := range podium {
+		// Make the index a podium position
+		position = rune(index+1) + '0'
 		for rowIndex, slice := range podium {
 			for _, str := range slice {
-				if hasChar(str, char) {
-					// newPodium[index][0] = str
+				if hasChar(str, position) {
 					podium[index][0], podium[rowIndex][0] = podium[rowIndex][0], podium[index][0]
 				}
 			}
