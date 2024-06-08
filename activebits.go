72b34624af4f76dac51e88d6c4a188d6abf74cd0
kapablanca <efstathiosgkazanakis@protonmail.com>
2024-06-08 15:33:34 +0300

    Updating activebits.

--- activebits.go
+++ activebits.go
@@ -1,9 +1,15 @@
 package piscine
 
+// Function that returns the number of active bits (bits with the value of 1)
+//  in the binary representation of an integer number.
 func ActiveBits(n int) int {
 	count := 0
 	for n != 0 {
-		count += n & 1
+		// Check the least significand bit with bitwise AND
+		if n&1 == 1 {
+			count += 1
+		}
+		// Right shift the bits of n
 		n >>= 1
 	}
 	return count
